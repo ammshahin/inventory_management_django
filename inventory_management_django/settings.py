656e2b495e6cf959355ecadff9b221604e1337e2
ammshahin <60013707+ammshahin@users.noreply.github.com>
2021-12-09 03:29:22 +0600

    save form and crispy

--- inventory_management_django/settings.py
+++ inventory_management_django/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'dashboard.apps.DashboardConfig',
+    'user.apps.UserConfig',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -101,6 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
