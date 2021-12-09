7a0116691ac3bb6c210c216f78d1e0081bd7cacb
ammshahin <60013707+ammshahin@users.noreply.github.com>
2021-12-10 02:40:29 +0600

    login reg logics

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,7 +3,7 @@ from django.urls.resolvers import URLPattern
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='dashboard-index'),
+    path('dashboard/', views.index, name='dashboard-index'),
     path('staffs/', views.staffs, name='dashboard-staffs'),
     path('product/', views.product, name='dashboard-product'),
     path('order/', views.order, name='dashboard-order')
