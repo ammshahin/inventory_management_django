dde0ddd2ff201e76e3b18003875591037fb81371
ammshahin <60013707+ammshahin@users.noreply.github.com>
2021-12-16 01:41:20 +0600

    staff profile page

--- user/models.py
+++ user/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    staff = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=15, null=True)
+    address = models.CharField(max_length=200, null=True)
+    image = models.ImageField(default='profile.jpg', upload_to = 'Profile_Images')
+
+    def __str__(self):
+        return f'{self.staff.username}-Profile'
