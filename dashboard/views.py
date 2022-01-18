06bcb32ec87410c0bf47b062cee81b35433db0ed
ammshahin <ammshahin@gmail.com>
2022-01-18 23:21:10 +0600

    add products

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,8 +1,9 @@
 from multiprocessing import context
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from .models import Product
+from .forms import CreateProductForm
 
 
 @login_required
@@ -16,8 +17,18 @@ def staffs(request):
 @login_required
 def product(request):
     products = Product.objects.all()
+    
+    if request.method == "POST":
+        form = CreateProductForm(request.POST)
+        if form.is_valid:
+            form.save()
+            return redirect('dashboard-product')
+    else:
+        form = CreateProductForm()
+
     context = {
         'products':products,
+        'form': form,
     }
     return render(request, 'dashboard/product.html',context)
 
